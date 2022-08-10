1cf92c7b810c321e99b27dee526148f14eee9a7d
Allison Ause <allison.ause@gmail.com>
2022-08-10 12:52:35 -0700

    added mock folder, info and test for oauth route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+jest.mock('../lib/services/github');
+
+describe('github oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET should login or create User from github oauth callback', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'test_github_user',
+      email: 'test@test.com',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
   afterAll(() => {
     pool.end();
