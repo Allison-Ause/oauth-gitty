3c267fd0960868e223e9ea12f932a9c28618eaf8
Allison Ause <allison.ause@gmail.com>
2022-08-12 11:31:10 -0700

    cleaned up last bits for eslint

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Posts = require('../lib/models/Posts');
 
 jest.mock('../lib/services/github');
 const agent = request.agent(app);
